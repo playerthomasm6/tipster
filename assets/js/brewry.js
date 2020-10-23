88eb8e4505f07b78f9c2614e1a39eae3514848ea
Matt Harris <711mattman@gmail.com>
2020-10-22 21:11:08 -0400

    Figured out the api situation

--- assets/js/brewry.js
+++ assets/js/brewry.js
@@ -1,13 +1,15 @@
-var queryURL = "http://sandbox-api.brewerydb.com/v2/beer/random?&apikey=c2c6d01c8bbe3417a46c5116a254b6af";
-
-$.ajax({
-    url: "https://limitless-tor-79246.herokuapp.com/cors",
-   method:"POST",
-   data: {
-       url: queryURL,
-        method: "GET",
-       key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
-   }}).then(function(response){
-       console.log(response);
-   });
-
+$(document).ready(function(){
+    // var APIKey = "4952f70986ff85965585395731129d9a";
+    var queryURL = "https://sandbox-api.brewerydb.com/v2/beer/random?key=4952f70986ff85965585395731129d9a";
+    $.ajax({
+             url: "https://limitless-tor-79246.herokuapp.com/cors",
+            method:"POST",
+            data: {
+                url: queryURL,
+                 method: "GET",
+                key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
+            }}).then(function(response){
+                console.log(response);
+                console.log(response.data.title);
+            })
+        });
--- assets/js/script.js
+++
@@ -1,2 +0,0 @@
-var queryURL = "http://sandboxapi.brewerydb.com/v2/beer/random/?key=2c6d01c8bbe3417a46c5116a254b6af"
-
