a52d9edd6f52e1267850286b9dd7b3091524adbe
Matt Harris <711mattman@gmail.com>
2020-10-22 21:03:23 -0400

    Fixed the cors error with the api

--- assets/js/brewry.js
+++ assets/js/brewry.js
@@ -1,7 +1,13 @@
+var queryURL = "http://sandbox-api.brewerydb.com/v2/beer/random?&apikey=c2c6d01c8bbe3417a46c5116a254b6af";
+
 $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(data){
-      var results =data;
-      console.log(data)
-  });
+    url: "https://limitless-tor-79246.herokuapp.com/cors",
+   method:"POST",
+   data: {
+       url: queryURL,
+        method: "GET",
+       key: "DB4868A0E1958DD298798EF1086835163AB3ED38D909D7A97BF3611FF87CD4DB"
+   }}).then(function(response){
+       console.log(response);
+   });
+
