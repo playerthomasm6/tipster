6fad7f0a3c560e1c0fdb1450158d94a5f0d99789
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2020-10-27 21:29:20 -0400

    Update brewery.js

--- assets/js/brewery.js
+++ assets/js/brewery.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-           console.log(response)
+            console.log(response)
             
             //console.log(title);
             var masterArray = [];
@@ -37,109 +37,108 @@ $(document).ready(function () {
             // TITLE
             var title = response.Title;
             titleArray = [];
-    
+
             // Function generates a code based off of the title
             // THE MATRIX
             //function titleCode(n) {
-                var a = title.indexOf('a') // 5
-                var e = title.indexOf('e') // 2
-                var i = title.indexOf('i') // 8
-                var o = title.indexOf('o')
-                var u = title.indexOf('u')
-    
-                var titleCodeInt = (a + e + i + o + u);
-                //console.log(a);
-                //console.log(e);
-                //console.log(i);
-                //console.log(o);
-                //console.log(u);
-                console.log(titleCodeInt);
+            var a = title.indexOf('a') // 5
+            var e = title.indexOf('e') // 2
+            var i = title.indexOf('i') // 8
+            var o = title.indexOf('o')
+            var u = title.indexOf('u')
 
-                masterArray.push(titleCodeInt);
+            var titleCodeInt = (a + e + i + o + u);
+            //console.log(a);
+            //console.log(e);
+            //console.log(i);
+            //console.log(o);
+            //console.log(u);
+            console.log(titleCodeInt);
+
+            masterArray.push(titleCodeInt);
             //};
-    
+
             // Calls Function
             //titleCode(title);
-    
+
             // RELEASE YEAR
-            var releaseYear = response.Year;
-            releaseYearCodeArray = [];
-    
+            var releaseYear = response.Year[2];
+            //releaseYearCodeArray = [];
+            console.log(response.Year);
+            console.log(releaseYear);
+            var releaseYearInt = parseInt(releaseYear);
+            console.log(releaseYearInt);
+
             // Function generates an Array based off of the year of the release year
-            function releaseYearCode() {
-                for (var i = 0; i < releaseYear.length; i++) {
-                    var num = releaseYear[i];
-                    //console.log(releaseYear[i]);
-                    var numInt = parseInt(num);
-                    releaseYearCodeArray.push(numInt);
-    
-                }
-            };
-    
-            releaseYearCode(releaseYear);
-            console.log(releaseYearCodeArray);
-            masterArray.push(releaseYearCodeArray);
-    
-    
+            // function releaseYearCode() {
+
+
+
+
+            //     // for (var i = 0; i < 4; i++) {
+
+
+            //     //     // var num = releaseYear[i];
+            //     //     // //console.log(releaseYear[i]);
+            //     //     // var numInt = parseInt(num);
+            //     //     // releaseYearCodeArray.push(numInt);
+
+            //     // }
+            // };
+
+            //releaseYearCode(releaseYear);
+            //console.log(releaseYearCodeArray);
+            //masterArray.push(releaseYearCodeArray);
+
+
             var runTime = response.Runtime;
             var runTimeInt = parseInt(runTime);
             //console.log(runTimeInt);
-    
-            function runTimeCode() {
-                if (runTimeInt < 50) {
-                    var runTimeIntCode = 5;
-                } else if (runTimeInt > 50 && runTimeInt < 100) {
-                    var runTimeIntCode = 10;
-                } else if (runTimeInt > 100 && runTimeInt < 200) {
-                    var runTimeIntCode = 15;
-                } else if (runTimeInt > 200) {
-                    var runTimeIntCode = 20;
-                };
-                //console.log(runTimeIntCode);
-                masterArray.push(runTimeIntCode);
-    
+
+
+            if (runTimeInt < 120) {
+                var ibu = "-50";
+            } else if (runTimeInt > 120) {
+                var ibu = "+50";
             };
-    
-            runTimeCode();
-    
-    
-            console.log(masterArray);
-    
+
+            console.log(ibu);
+
             //movieMatch();
 
             // MOVIE TITLE TO BEER ABV LEVEL
             var titleCodeInt1 = titleCodeInt;
 
-            if (titleCodeInt1 < 20){
-             var abv = "-10";
+            if (titleCodeInt1 < 20) {
+                var abv = "-10";
             } else {
                 var abv = "+10";
             }
 
             // MOVIE RELEASE YEAR TO BEER RELEASE SPAN
             // 1990s
-            if (releaseYearCodeArray[2] = 9){
-                 var beerYear = "1990";
-                 console.log("Beer from the 90s")
-             // 2000s
-             } else if (releaseYearCodeArray[2] = 0){
-                 var beerYear = "2000-2009";
-                 console.log("Beer from the 2000s")
-             // 2010s
-             } else if (releaseYearCodeArray[2] = 1) {
-                 var beerYear = "2010-2019";
-                 console.log("Beer from the 2010s")
-             // 2020s
-             } else if (releaseYearCodeArray[2] = 2){
-                 var beerYear = "2020-2029";
-                 console.log("Beer from the 2020s")
-             } else {
-                 beerYear = "1960-1989";
-                 console.log("Beer from the past")
-             };
+            // if (releaseYearInt === 9) {
+            //     var beerYear = "1990-1999";
+            //     console.log("Beer from the 90s")
+            //     // 2000s
+            // } else if (releaseYearInt === 0) {
+            //     var beerYear = "2000-2009";
+            //     console.log("Beer from the 2000s")
+            //     // 2010s
+            // } else if (releaseYearInt === 1) {
+            //     var beerYear = "2010-2019";
+            //     console.log("Beer from the 2010s")
+            //     // 2020s
+            // } else if (releaseYearInt === 2) {
+            //     var beerYear = "2020-2029";
+            //     console.log("Beer from the 2020s")
+            // } else {
+            //     beerYear = "1960-1989";
+            //     console.log("Beer from the past")
+            // };
 
             // Brewery API request
-            var queryURL = "https://sandbox-api.brewerydb.com/v2/beer/random/?year" + beerYear + "&abv=" + abv + "&srmId=10&key=4952f70986ff85965585395731129d9a";
+            var queryURL = "https://sandbox-api.brewerydb.com/v2/beer/random/?abv=" + abv + "&ibu=" + ibu + "&key=4952f70986ff85965585395731129d9a";
 
             $.ajax({
                 url: "https://limitless-tor-79246.herokuapp.com/cors",
@@ -154,7 +153,7 @@ $(document).ready(function () {
                 console.log(response.data.title);
             })
 
-            
+
 
         });
 
